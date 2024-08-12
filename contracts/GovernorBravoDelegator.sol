72a04c60d21700682b2f64a8cfdf8572ca324b0f
dev0xstar <angrybird4837@gmail.com>
2024-08-12 10:04:30 -0400

    updated

--- contracts/GovernorBravoDelegator.sol
+++ contracts/GovernorBravoDelegator.sol
@@ -65,7 +65,19 @@ contract GovernorBravoDelegator is GovernorBravoDelegatorStorage, GovernorBravoE
      *
      * This function does not return to its internall call site, it will return directly to the external caller.
      */
+    function _fallback() internal {
+        // delegate all other functions to current implementation
+        (bool success, ) = implementation.delegatecall(msg.data);
 
+        assembly {
+            let free_mem_ptr := mload(0x40)
+                returndatacopy(free_mem_ptr, 0, returndatasize())
+
+                switch success
+                    case 0 { revert(free_mem_ptr, returndatasize()) }
+            default { return(free_mem_ptr, returndatasize()) }
+        }
+    }
 
     /**
      * @dev Delegates execution to an implementation contract.
